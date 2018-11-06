f6d2faf592203ba5b45cddedd00644a9add29fdc
Teo Espero <38012835+teoespero@users.noreply.github.com>
2018-11-06 10:25:32 -0800

    Add files via upload
    HRPosAssignment:
    PosID - use the position Slot# not the PCID
    CalendarID – need the work calendar description
    ***the following needs to come from the employee Comp Detail record for each position slot#
    SalaryScheduleID
    SalaryRowID
    SalaryColID

--- HRPosDtl.sql
+++ HRPosDtl.sql
@@ -2,7 +2,6 @@
 	
 	HRPosDtl
 	The HRPosDtl table tracks the details of a position.
-
 */
 
 select 
@@ -13,21 +12,29 @@ from tblDistrict
 
 select 
 	(select DistrictId from tblDistrict) as OrgId,
-	pcd.PositionControlID as PosID,
+	pcd.SlotNum as PosID,
 	CONVERT(VARCHAR(10), pcd.EffectiveDate, 110) as DateFrom,
 	CONVERT(VARCHAR(10), pcd.InactiveDate, 110) as DateThru,
 	pcd.FTE as FTEAuthorized,
 	pcd.SiteID,
-	jt.SubClassificationID as JobCategoryId,
-	jt.JobTitleID as JobClassId,
+	si.SiteName,
+	cl.ClassificationID as JobCategoryId,
+	cl.ClassDescription as JobCategory,
+	su.SubClassificationID as JobClassId,
+	su.SubClassDesc as JobClass,
 	null as BargUnitId,
 	pcd.SupervisorEmployeeID as PosIdSupervisor,
 	null as DivisionID,
 	null as AcademicDeptCode,
 	pcd.Comments as Comment,
-	cl.ClassDescription,
-	su.SubClassDesc,
-	jt.JobTitle
+	jt.JobTitle,
+	ct.CompType,
+	ser.Series,
+	smg.GroupName,
+	sm.RowNumber,
+	sm.ColNumber,
+	sm.StepColumn,
+	sm.Value
 from tblPositionControlDetails pcd
 inner join
 	tblJobTitles jt
@@ -39,6 +46,25 @@ inner join
 inner join
 	tblClassifications cl
 	on cl.ClassificationID = su.scClassificationID
-
-
+left join
+	tblSite si
+	on pcd.SiteID = si.SiteID
+left join
+	tblCompDetails cd
+	on pcd.PositionControlID = cd.cdPositionControlID
+	and cd.FiscalYear = 2018
+	and cd.InactiveDate is null
+left join
+	tblSalaryMatrix sm
+	on sm.SalaryMatrixID = cd.SalaryMatrixID
+left join
+	tblSalaryMatrixGroup smg
+	on sm.mxGroup = smg.SalaryMatrixGroupID
+left join
+	tblSalaryMatrixSeries ser
+	on ser.mxSeriesID = sm.SeriesID
+left join
+	tblCompType ct
+	on ct.CompTypeID = sm.smCompTypeID
+order by pcd.SlotNum asc
 
